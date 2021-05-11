17540caefe9d01531952113783c3e4b000f4c4c3
Santiago Bedoya <santiago.bedoya@wolox.co>
2021-05-11 17:27:45 -0500

    Added get geek quote service and exposed via /weets route. Added the errors corresponding to the requested API

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,10 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
+const { getQuote } = require('./controllers/weets');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  // app.get('/endpoint/get/path', [], controller.methodGET);
+  app.get('/weets', getQuote);
   // app.put('/endpoint/put/path', [], controller.methodPUT);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
 };

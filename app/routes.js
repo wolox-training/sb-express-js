4f07cd9934db9431e3c10d208f8f2168e3128016
Santiago Bedoya <santiago.bedoya@wolox.co>
2021-05-14 15:36:03 -0500

    Added middlewares to validate email and password format and implemented into create users service

--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,11 @@
 const { healthCheck } = require('./controllers/healthCheck');
 const { getQuote } = require('./controllers/weets');
 const { createUser } = require('./controllers/users');
+const { validateEmail, validatePassword } = require('./middlewares/users');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/weets', getQuote);
-  app.post('/users', createUser);
+  app.post('/users', validatePassword, validateEmail, createUser);
   // app.post('/endpoint/post/path', [], controller.methodPOST);
 };

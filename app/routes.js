4845fd135e6a726081d76ce9dd66244033b4e679
Santiago Bedoya <santiago.bedoya@wolox.co>
2021-05-19 09:42:48 -0500

    Removed weets controller and route

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,6 @@
 // const controller = require('./controllers/controller');
 const { healthCheck } = require('./controllers/healthCheck');
-const { getQuote } = require('./controllers/weets');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  app.get('/weets', getQuote);
 };

efac9edb4f1884f8a72b7271b9a01fa8a76540f8
Santiago Bedoya <santiago.bedoya@wolox.co>
2021-05-26 22:53:34 -0500

    Fixed case converter with arrays

--- app/helpers/caseConverter.js
+++ app/helpers/caseConverter.js
@@ -4,11 +4,15 @@ const camelCaseKeytoSnakeCase = str => str.replace(/[A-Z]/g, $1 => `_${$1.toLowe
 const snakeCaseKeytoCamelCase = str => str.replace(/(_[a-z])/g, $1 => $1.toUpperCase().replace('_', ''));
 
 const generalCaseConverter = (element, singleConverterFunction) => {
-  if (Array.isArray(element)) return element.map(object => generalCaseConverter(object));
-  else if (isJSON(element)) {
+  if (Array.isArray(element)) {
+    return element.map(object => generalCaseConverter(object, singleConverterFunction));
+  } else if (isJSON(element)) {
     const formattetObject = {};
     Object.keys(element).forEach(key => {
-      formattetObject[singleConverterFunction(key)] = generalCaseConverter(element[key]);
+      formattetObject[singleConverterFunction(key)] = generalCaseConverter(
+        element[key],
+        singleConverterFunction
+      );
     });
     return formattetObject;
   }
